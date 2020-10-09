cf3edc9f2a65ab338396fec99ac372c5bb9e62a9
alsruddl0805 <alsruddl0805@naver.com>
2020-10-09 18:40:16 +0900

    bbs_pagerStyle

--- assets/script/bbs.js
+++ assets/script/bbs.js
@@ -1,6 +1,5 @@
 // bbs 보완
-// 1. 페이지버튼 클릭 시 class로 색 변화주기
-// 2. 공지사항 번호 js로 구현하기
+// 공지사항 번호 js로 구현하기
 
 // mainBar
 
@@ -10,5 +9,18 @@ $("#mainBar>li:nth-child(2),#mainBar>li:nth-child(3)").click(function(){
     $(this).children(".subBar").toggleClass('subBarStyle');
   });
 
-// pager
-// 포커스 되었을때만 class적용
+// pager hover style
+
+let pageBtn = document.querySelectorAll("#pageBtn li a");
+
+for (let i=0; i<pageBtn.length; i++) {
+  pageBtn[i].addEventListener("focusin",function(){
+    this.classList.add("pager");
+  });
+}
+
+for (let i=0; i<pageBtn.length; i++) {
+  pageBtn[i].addEventListener("focusout",function(){
+    this.classList.remove("pager");
+  });
+}
