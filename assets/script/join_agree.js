3b9eb4d2093ea2f6bffed4fefaa693acb3662040
alsruddl0805 <alsruddl0805@naver.com>
2020-10-10 21:08:45 +0900

    join_agree수정완료

--- assets/script/join_agree.js
+++ assets/script/join_agree.js
@@ -1,5 +1,3 @@
-// 보완작업 - 전체동의 재 클릭으로 비활성화 시에 동의하지 않음으로 넘어가기
-
 // mainBar
 
 $("#mainBar>li:nth-child(2),#mainBar>li:nth-child(3)").click(function(){
@@ -9,18 +7,20 @@ $("#mainBar>li:nth-child(2),#mainBar>li:nth-child(3)").click(function(){
 });
 
 // 전체동의 버튼 활성화
+let allCheck = document.querySelector("#allCheck");
 
-let allChkBtn = document.querySelector("#allCheck");
-let chkBtn = document.querySelectorAll(".agree");
+allCheck.addEventListener("click",function() {
+  let allChkVal = allCheck.checked;
+  let partnerAgree = document.querySelector("#partnerAgree");
+  let useAgree = document.querySelector("#useAgree");
+  let consignmentAgree = document.querySelector("#consignmentAgree");
 
-allChkBtn.addEventListener("click",function() {
-    for (let i=0; i<chkBtn.length; i++) {
-      chkBtn[i].checked = allChkBtn;
-    }
+  partnerAgree.checked = allChkVal;
+  useAgree.checked = allChkVal;
+  consignmentAgree.checked = allChkVal;
 });
 
 // 동의하지 않음 체크시 전체동의 비활성화
-
 let partnerDisagree = document.querySelector("#partnerDisagree");
 let useDisagree = document.querySelector("#useDisagree");
 let consignmentDisagree = document.querySelector("#consignmentDisagree");
@@ -32,26 +32,38 @@ consignmentDisagree.addEventListener("click",disagreeChk);
 
 function disagreeChk() {
   if (partnerDisagree.checked || useDisagree.checked || consignmentDisagree.checked) {
-    allChkBtn.checked = false;
+    allCheck.checked = false;
   } else {
-    allChkBtn.checked = true;
+    allCheck.checked = true;
   }
 }
 
 // 동의함 버튼 클릭 시 전체동의 활성화
-let partnerAgree = document.querySelector("#partnerAgree");
-let useAgree = document.querySelector("#useAgree");
-let consignmentAgree = document.querySelector("#consignmentAgree");
-
 partnerAgree.addEventListener("click",agreeChk);
 useAgree.addEventListener("click",agreeChk);
 consignmentAgree.addEventListener("click",agreeChk);
 
-
 function agreeChk() {
   if (partnerAgree.checked && useAgree.checked && consignmentAgree.checked) {
-    allChkBtn.checked = true;
+    allCheck.checked = true;
   } else {
-    allChkBtn.checked = false;
+    allCheck.checked = false;
   }
 }
+
+// 동의버튼 유효성 검사
+let confirm = document.querySelector("#confirm");
+confirm.addEventListener("click",function(){
+  if (partnerAgree.checked == false) {
+    alert("아웃백 회원 약관에 대한 동의는 필수 항목입니다.");
+    partnerAgree.focus();
+  } else if (useAgree.checked == false) {
+    alert("개인정보 수집 및 이용에 대한 동의는 필수 항목입니다.");
+    useAgree.focus();
+  } else if (consignmentAgree.checked == false) {
+    alert("개인정보 취급 위탁에 대한 동의는 필수 항목입니다.");
+  } else {
+    document.querySelector("form").action = "../pager/join_userInfo.html";
+    document.querySelector("form").submit();
+  }
+});
