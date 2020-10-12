d73bb7b5d20753d2707042d2257e7168903a1d04
alsruddl0805 <alsruddl0805@naver.com>
2020-10-12 18:04:08 +0900

    join_userInfo_randomNum

--- assets/script/join_userInfo.js
+++ assets/script/join_userInfo.js
@@ -10,8 +10,8 @@ $("#mainBar>li:nth-child(2),#mainBar>li:nth-child(3)").click(function(){
 
 let Email = document.querySelector("#select_email");
 Email.addEventListener("change",function(){
-  let emailVal = Email.value;
-  document.querySelector("#emailAddress").value = emailVal;
+  let emailVal = Email;
+  document.querySelector("#emailAddress") = emailVal;
       if (emailVal == "") {
           document.querySelector("#emailAddress").readOnly = false;
           document.querySelector("#emailAddress").focus();
@@ -46,8 +46,8 @@ for (let m=1; m<=12; m++) {
 lastday();
 
 function lastday(){ //년과 월에 따라 마지막 일 구하기 
-    let yearVal= Year.value;
-    let monthVal= Month.value;
+    let yearVal= Year;
+    let monthVal= Month;
     let day=new Date(new Date(yearVal,monthVal,1)-86400000).getDate(); 
     let dayindex_len=document.querySelector("#select_day").length;
     if (day>dayindex_len) {
@@ -60,3 +60,35 @@ function lastday(){ //년과 월에 따라 마지막 일 구하기
         }
     } 
 }
+
+// 모바일 카드 랜덤번호 추출하기
+
+let res_01 = document.querySelector("#res_01");
+let res_02 = document.querySelector("#res_02");
+let res_03 = document.querySelector("#res_03");
+let res_04 = document.querySelector("#res_04");
+
+let mobileCardBtn = document.querySelector("#mobileCardBtn");
+mobileCardBtn.addEventListener("click",function(){
+  let min = 1111;
+  let max = 9999;
+
+  let ranNum_01 = Math.floor(Math.random()*(max-min+1))+min;
+  let ranNum_02 = Math.floor(Math.random()*(max-min+1))+min;
+  let ranNum_03 = Math.floor(Math.random()*(max-min+1))+min;
+  let ranNum_04 = Math.floor(Math.random()*(max-min+1))+min;
+
+  res_01 = ranNum_01;
+  res_02 = ranNum_02;
+  res_03 = ranNum_03;
+  res_04 = ranNum_04;
+});
+
+let mobileCardUpdate = document.querySelector("#mobileCardUpdate");
+mobileCardUpdate.addEventListener("click",function(){
+    if (res_01 == "" || res_02 == "" || res_03 == "" || res_04 == "") {
+      alert("카드번호를 정확하게 입력해주세요.");
+    } else {
+      alert("카드 등록이 완료되었습니다.")
+    }
+});
