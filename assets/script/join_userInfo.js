bb54965b17393ebd311f83da701d8709a52cd25d
alsruddl0805 <alsruddl0805@naver.com>
2020-10-12 16:28:26 +0900

    join_userInfo_진행중

--- assets/script/join_userInfo.js
+++ assets/script/join_userInfo.js
@@ -1,7 +1,21 @@
-//mainBar
+// mainBar
 
 $("#mainBar>li:nth-child(2),#mainBar>li:nth-child(3)").click(function(){
     $(this).children(".subBar").stop().slideToggle(500);
     $(this).toggleClass('mainBarStyle');
     $(this).children(".subBar").toggleClass('subBarStyle');
   });
+
+// email select 
+
+let Email = document.querySelector("#select_email");
+Email.addEventListener("change",function(){
+  let emailVal = Email.value;
+  document.querySelector("#emailAddress").value = emailVal;
+      if (emailVal == "") {
+          document.querySelector("#emailAddress").readOnly = false;
+          document.querySelector("#emailAddress").focus();
+      } else {
+          document.querySelector("#emailAddress").readOnly = true;     
+      }
+});
