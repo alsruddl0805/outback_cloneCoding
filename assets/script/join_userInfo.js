437ecd44c72d846ef2c3a815e6b9015ded0f9d53
alsruddl0805 <alsruddl0805@naver.com>
2020-10-12 16:39:54 +0900

    join_userInfo_작업진행중

--- assets/script/join_userInfo.js
+++ assets/script/join_userInfo.js
@@ -19,3 +19,44 @@ Email.addEventListener("change",function(){
           document.querySelector("#emailAddress").readOnly = true;     
       }
 });
+
+// birth select
+
+let Year= document.querySelector("#select_year"); 
+let Month= document.querySelector("#select_month");
+Year.addEventListener("change",lastday);
+Month.addEventListener("change",lastday);
+
+let start_year="1998";
+let today= new Date(); 
+let today_year= today.getFullYear(); 
+let index=0; 
+
+for (let y=start_year; y<=today_year; y++) { //start_year ~ 현재 년도 
+    document.querySelector("#select_year").options[index] = new Option(y, y); 
+    index++; 
+  } 
+
+index=0; 
+
+for (let m=1; m<=12; m++) { 
+    document.querySelector("#select_month").options[index] = new Option(m, m); index++; 
+} 
+
+lastday();
+
+function lastday(){ //년과 월에 따라 마지막 일 구하기 
+    let yearVal= Year.value;
+    let monthVal= Month.value;
+    let day=new Date(new Date(yearVal,monthVal,1)-86400000).getDate(); 
+    let dayindex_len=document.querySelector("#select_day").length;
+    if (day>dayindex_len) {
+        for(let i=(dayindex_len+1); i<=day; i++){ 
+          document.querySelector("#select_day").options[i-1] = new Option(i, i); 
+        } 
+    } else if (day<dayindex_len) { 
+        for (let i=dayindex_len; i>=day; i--) { 
+          document.querySelector("#select_day").options[i]=null; 
+        }
+    } 
+}
