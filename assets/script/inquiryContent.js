5f8facc41e398bc9865aafa99678c21a12ebc4f6
alsruddl0805 <alsruddl0805@naver.com>
2020-10-14 17:46:23 +0900

    inquiryContent_작업완료

---
+++ assets/script/inquiryContent.js
@@ -0,0 +1,7 @@
+// mainBar
+
+$("#mainBar>li:nth-child(2),#mainBar>li:nth-child(3)").click(function(){
+    $(this).children(".subBar").stop().slideToggle(500);
+    $(this).toggleClass('mainBarStyle');
+    $(this).children(".subBar").toggleClass('subBarStyle');
+  });
