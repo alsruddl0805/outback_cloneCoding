db18bb082ba3eacdf3f72b0a6c3087a8bab0f053
alsruddl0805 <alsruddl0805@naver.com>
2020-10-14 20:36:37 +0900

    sns주소연동

--- assets/script/common.js
+++ assets/script/common.js
@@ -19,3 +19,26 @@ topBtn.addEventListener("click",goTop);
 function goTop() {
     $('html, body').stop().animate({scrollTop: 0}, delay);
 }
+
+// naver Reserve 
+
+let naverReserve = document.querySelector("#addMenu ul li:first-child a");
+naverReserve.href = "https://map.naver.com/v5/search/%EC%95%84%EC%9B%83%EB%B0%B1?placeSearchOption=fromNxList=true%26noredirect=1%26entry=pll&c=14108429.1958773,4509942.3586165,13,0,0,0,dh";
+
+// save point 
+
+
+
+// sns 주소 연동
+
+let instagram = document.querySelector("#snsIcon a:first-child");
+instagram.href = "https://www.instagram.com/outbackkorea/";
+
+let facebook = document.querySelector("#snsIcon a:nth-child(2)");
+facebook.href = "https://www.facebook.com/koreaoutback";
+
+let youtube = document.querySelector("#snsIcon a:nth-child(3)");
+youtube.href = "https://www.youtube.com/user/outbackkorea";
+
+let kakao = document.querySelector("#snsIcon a:last-child");
+kakao.href = "https://pf.kakao.com/_axcnxah";
