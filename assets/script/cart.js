33b29ea7a0c1eefd1b84f77f6a501af641bb867a
alsruddl0805 <alsruddl0805@naver.com>
2020-10-21 09:45:41 +0900

    cart_수량변경_작업진행중

--- assets/script/cart.js
+++ assets/script/cart.js
@@ -33,22 +33,26 @@ for (let i=0; i<product_chk.length; i++) {
 
 // 선택삭제 활성화
 
+let totalPrice = document.querySelectorAll(".totalPrice");
+
 delBtn.addEventListener("click",function(){
   if (confirm("선택하신 상품을 삭제하시겠습니까?")) {
-    if (product_chk[0].checked == false && product_chk[1].checked == false) {
-      alert("선택된 상품이 없습니다.");
-      return;
-    } else if (allCheck.checked) { // 전체선택일때 2개 다 삭제
-        $("#cartTable tr:nth-child(2) td").remove();
-        $("#cartTable tr:nth-child(3) td").remove();
-    } else if (product_chk[0].checked) { // line 1 선택시 해당 라인 삭제
-        $("#cartTable tr:nth-child(2) td").remove();
-    } else if (product_chk[1].checked) { // line 2 선택시 해당 라인 삭제
-        $("#cartTable tr:nth-child(3) td").remove();
+    for (let i=0; i<totalPrice.length; i++) {
+      if (product_chk[0].checked == false && product_chk[1].checked == false) {
+        alert("선택된 상품이 없습니다.");
+        return;
+      } else if (allCheck.checked) { // 전체선택일때 2개 다 삭제
+          $("#cartTable tr:nth-child(2) td").remove();
+          $("#cartTable tr:nth-child(3) td").remove();
+          totalPrice[i].value = 0;
+      } else if (product_chk[0].checked) { // line 1 선택시 해당 라인 삭제
+          $("#cartTable tr:nth-child(2) td").remove();
+      } else if (product_chk[1].checked) { // line 2 선택시 해당 라인 삭제
+          $("#cartTable tr:nth-child(3) td").remove();
+      }
     }
   }
 });
-
 
 // minus,plus 버튼 활성화 & 수량 변동
 
@@ -58,74 +62,115 @@ let plusBtn = document.querySelectorAll(".plusBtn");
 let product_cnt = document.querySelectorAll(".product_cnt");
 let product_price = document.querySelectorAll(".product_price");
 let basePrice = document.querySelectorAll(".basePrice");
-let totalPrice = document.querySelectorAll(".totalPrice");
+
 
 // minus
 
 for (let i=0; i<minusBtn.length; i++) {
-  minusBtn[i].addEventListener("click",function(){
+  minusBtn[i].addEventListener("click",minus_cnt);
     
+function minus_cnt() {
   let sum = parseInt(product_price[i].value);
-    
-    if (product_cnt[i].value > 1) { // 수량 감소
-      product_cnt[i].value--; 
-      sum -= parseInt(basePrice[i].innerText); // 수량에 따른 금액 감소
-      product_price[i].value = sum;
-    } else {
-      alert("최소수량은 1개입니다.");
-      product_cnt[i].value = 1;
-    } 
+  
+  if (product_cnt[i].value > 1) { // 수량 감소
+    product_cnt[i].value--; 
+    sum -= parseInt(basePrice[i].innerText); // 수량에 따른 금액 감소
+    product_price[i].value = sum;
+  } else {
+    alert("최소수량은 1개입니다.");
+    product_cnt[i].value = 1;
+  } 
 
-    product_total_price();
+  // product_total_price();
+}
 
-  });
 }
 
 // plus
 
 for (let i=0; i<plusBtn.length; i++) {
-  plusBtn[i].addEventListener("click", function() {
-  
+plusBtn[i].addEventListener("click",plus_ban);
+plusBtn[i].addEventListener("click",plus_cnt);
+
+function plus_ban() {
+  if (allCheck.checked == true || product_chk[0].checekd == true || product_chk[0].checekd == true) {
+    plus_cnt();
+  } else {
+    return false;
+  }
+}
+
+function plus_cnt() {
   let sum = parseInt(product_price[i].value);
-  
-    if (product_cnt[i].value < 10) { // 수량증가
-      product_cnt[i].value++;
-      sum += parseInt(basePrice[i].innerText);// 수량에 따른 금액 증가
-      product_price[i].value = sum;
-    } else {
-      alert("최대수량은 10개입니다.\n10개 이상은 가까운 매장으로 전화주문 부탁드립니다.");
-      product_cnt[i].value = 10;
-    }
 
-    product_total_price();
+  if (product_cnt[i].value < 10) { // 수량증가
+    product_cnt[i].value++;
+    sum += parseInt(basePrice[i].innerText);// 수량에 따른 금액 증가
+    product_price[i].value = sum;
+  } else {
+    alert("최대수량은 10개입니다.\n10개 이상은 가까운 매장으로 전화주문 부탁드립니다.");
+    product_cnt[i].value = 10;
+  }
+  product_total_price();
+}
 
-  });   
-};
-
+}
 
  // 각 상품 금액 총 합계 추출
 
 function product_total_price() {
-  let addPrice = 2500;
+  let addPrice = document.querySelector(".addPrice").value;
+  addPrice = parseInt(addPrice);
   let totalSum = 0;
 
   for (let i=0; i<product_price.length; i++) { // 각 합계
     totalSum += parseInt(product_price[i].value);
   }
-  
+
   for (let i=0; i<totalPrice.length; i++) { // 총 합계
     let totalSumAdd = totalSum + addPrice;
-    totalPrice[0].value = totalSum.toLocaleString();
-    totalPrice[1].value = totalSumAdd.toLocaleString();
+    totalPrice[0].value = totalSum;
+    totalPrice[1].value = totalSumAdd;
   }
 }
 
 product_total_price();
 
+// 체크박스 해제 시 금액 변동
 
+function priceChk() {
+  for (let i=0; i<product_chk.length; i++) {
+    product_chk[i].addEventListener("change",function(){
 
+    let addPrice = document.querySelector(".addPrice").value;
+    addPrice = parseInt(addPrice);
+  
+    if (product_chk[0].checked == false && product_chk[1].checked == false) {
+      totalPrice[0].value = 0;
+      totalPrice[1].value = 0;
+      return;
+    } else if (product_chk[0].checked == false) {
+      totalPrice[0].value = product_price[1].value;
+      totalPrice[1].value = parseInt(product_price[1].value) + addPrice;
+    } else if (product_chk[1].checked == false) {
+      totalPrice[0].value = product_price[0].value;
+      totalPrice[1].value = parseInt(product_price[0].value) + addPrice;
+    } else if (product_chk[0].checked && product_chk[1].checked) {
+      totalPrice[0].value = parseInt(product_price[0].value) + parseInt(product_price[1].value);
+      totalPrice[1].value = parseInt(product_price[0].value) + parseInt(product_price[1].value) + addPrice;
+    }
 
+    });
+  }
+}
 
+priceChk();
+
+// 전체선택 해제 시 금액 변동
+
+allCheck.addEventListener("change",function(){
+
+});
 
 
 // 주문하기 버튼 활성화
