27d50175e41d42d14625f3707c45dea72dcffedd
alsruddl0805 <alsruddl0805@naver.com>
2020-10-21 14:48:04 +0900

    cart_수량변경_작업진행중

--- assets/script/cart.js
+++ assets/script/cart.js
@@ -1,60 +1,43 @@
-// cart 보완
-// 1. 선택삭제 alert 뜨는 if 조건문 검사
-// 2. checked 되어야결제 예상금액 확인 가능
-
 
 let allCheck = document.querySelector("#allCheck");
 let product_chk = document.querySelectorAll(".product_chk");
 let delBtn = document.querySelector("#chkDelete");
+let totalPrice = document.querySelectorAll(".totalPrice");
 allCheck.checked = true;
 
-// 전체선택 활성화
 
-allCheck.addEventListener("click",function() {
-  let allChkVal = allCheck.checked;
+// 1. 체크박스 기능 구현하기
 
-  for (let i=0; i<product_chk.length; i++) {
+for (let i=0; i<product_chk.length; i++) {
+
+  // 전체선택 동의 활성화
+  allCheck.addEventListener("click",function() {
+    let allChkVal = allCheck.checked;    
     product_chk[i].checked = allChkVal;
+  });
+
+  // 전체선택 해제 시 금액 초기화
+  allCheck.addEventListener("change",function(){
+  if (allCheck.checked == true) {
+    product_total_price();
+  } else {
+    totalPrice[i].value = 0; 
   }
 });
 
-// 개별선택 활성화
+// 개별선택 동의 활성화
+product_chk[i].addEventListener("click",function() {
+  if (product_chk[0].checked && product_chk[1].checked) {
+    allCheck.checked = true;
+  } else {
+    allCheck.checked = false;
+  }
+});
+product_chk[i].checked = true;
 
-for (let i=0; i<product_chk.length; i++) { 
-  product_chk[i].addEventListener("click",function() {
-    if (product_chk[0].checked && product_chk[1].checked) {
-      allCheck.checked = true;
-    } else {
-      allCheck.checked = false;
-    }
-  });
-  product_chk[i].checked = true;
 }
 
-// 선택삭제 활성화
-
-let totalPrice = document.querySelectorAll(".totalPrice");
-
-delBtn.addEventListener("click",function(){
-  if (confirm("선택하신 상품을 삭제하시겠습니까?")) {
-    for (let i=0; i<totalPrice.length; i++) {
-      if (product_chk[0].checked == false && product_chk[1].checked == false) {
-        alert("선택된 상품이 없습니다.");
-        return;
-      } else if (allCheck.checked) { // 전체선택일때 2개 다 삭제
-          $("#cartTable tr:nth-child(2) td").remove();
-          $("#cartTable tr:nth-child(3) td").remove();
-          totalPrice[i].value = 0;
-      } else if (product_chk[0].checked) { // line 1 선택시 해당 라인 삭제
-          $("#cartTable tr:nth-child(2) td").remove();
-      } else if (product_chk[1].checked) { // line 2 선택시 해당 라인 삭제
-          $("#cartTable tr:nth-child(3) td").remove();
-      }
-    }
-  }
-});
-
-// minus,plus 버튼 활성화 & 수량 변동
+// 2. minus,plus 버튼 활성화 & 수량 변동
 
 let minusBtn = document.querySelectorAll(".minusBtn");
 let plusBtn = document.querySelectorAll(".plusBtn");
@@ -81,7 +64,7 @@ function minus_cnt() {
     product_cnt[i].value = 1;
   } 
 
-  // product_total_price();
+  product_total_price();
 }
 
 }
@@ -89,16 +72,7 @@ function minus_cnt() {
 // plus
 
 for (let i=0; i<plusBtn.length; i++) {
-plusBtn[i].addEventListener("click",plus_ban);
 plusBtn[i].addEventListener("click",plus_cnt);
-
-function plus_ban() {
-  if (allCheck.checked == true || product_chk[0].checekd == true || product_chk[0].checekd == true) {
-    plus_cnt();
-  } else {
-    return false;
-  }
-}
 
 function plus_cnt() {
   let sum = parseInt(product_price[i].value);
@@ -116,7 +90,7 @@ function plus_cnt() {
 
 }
 
- // 각 상품 금액 총 합계 추출
+ // 3. 각 상품 금액 총 합계 추출
 
 function product_total_price() {
   let addPrice = document.querySelector(".addPrice").value;
@@ -136,7 +110,7 @@ function product_total_price() {
 
 product_total_price();
 
-// 체크박스 해제 시 금액 변동
+// 4. 체크박스 해제 시 금액 변동
 
 function priceChk() {
   for (let i=0; i<product_chk.length; i++) {
@@ -166,18 +140,15 @@ function priceChk() {
 
 priceChk();
 
-// 전체선택 해제 시 금액 변동
-
-allCheck.addEventListener("change",function(){
-
-});
-
-
-// 주문하기 버튼 활성화
+// 5. 주문하기 버튼 활성화
 
 let orderBtn = document.querySelector("#orderBtn");
 orderBtn.addEventListener("click",function(){
     if (confirm("전체 상품을 주문하시겠습니까?")) {
-      alert("주문이 완료 되었습니다.");
+      if (product_chk[0].checked == false && product_chk[1].checked == false) {
+        alert("선택된 상품이 없습니다.");
+      } else {
+        alert("주문이 완료되었습니다.")
+      }
     }
 });
