2f3bd85a3cb412d2141499f2d581bb9dab3643a8
alsruddl0805 <alsruddl0805@naver.com>
2020-10-22 21:59:02 +0900

    manager_select_작업완료

--- assets/script/inquiry_main.js
+++ assets/script/inquiry_main.js
@@ -21,34 +21,9 @@ function login() {
       } else if (uPW.value.trim() == "") {
           alert("비밀번호를 입력하세요.");
           uPW.focus();
-      } else if (uID.value.trim() == "123" && uPW.value.trim() == "12345") {
-          alert("관리자로 로그인 되었습니다.");
-          document.querySelector("form").action = "../pager/manager.html";
-          document.querySelector("form").submit();
       } else {
           alert("로그인 되었습니다.");
           document.querySelector("form").action = "../pager/myoutback.html";
           document.querySelector("form").submit();
       }
-  }
-
-  // 관리자 페이지 이동 버튼
-
-  let manageBtn = document.querySelector("#moveManager");
-  manageBtn.addEventListener("click",moveManager);
-
-  function moveManager() {
-    if (uID.value.trim() == "") {
-        alert("관리자 ID : 123 \n관리자 PW : 12345");
-        uID.focus();
-    } else if (uPW.value.trim() == "") {
-        alert("관리자 ID : 123 \n관리자 PW : 12345");
-        uPW.focus();
-    } else if (uID.value.trim() == "123" && uPW.value.trim() == "12345") {
-        alert("관리자로 로그인 되었습니다.");
-        document.querySelector("form").action = "../pager/manager.html";
-        document.querySelector("form").submit();
-    } else {
-        alert("관리자 ID,PW를 확인해주세요.");
-    }     
   }
